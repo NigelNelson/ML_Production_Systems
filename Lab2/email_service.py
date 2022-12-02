6e615b1c802b2ae6c37cfe4e6411a9e5ce874bf6
Collin Quinn <collinquinn14@gmail.com>
2022-12-02 08:56:14 -0600

    new

--- Lab2/email_service.py
+++ Lab2/email_service.py
@@ -4,12 +4,12 @@ import psycopg2
 import os
 import sys
 
-
-
 app = Flask(__name__)
 
-
 def get_db_connection():
+    """
+    TODO - read in using environment variables
+    """
 
     # # try: 
     # POSTGRES_DATABASE=os.environ.get('POSTGRES_DATABASE')
@@ -35,18 +35,90 @@ def get_db_connection():
 
 @app.route('/email', methods=['POST'])
 def post_email():
-    
+    int_random = random.randint(1000, 9999)
+
     request_data = request.get_json()
     request_data_to = request_data["to"]
     request_data_from = request_data["from"]
     request_data_subject = request_data["subject"]
     request_data_body = request_data["body"]
 
-    int_random = random.randint(1000, 9999)
 
     return jsonify(email_id=int_random)
 
 
+@app.route('/mailbox/email/<email_id:int>', methods=['GET'])
+def get_email_by_id():
+    """
+    Returns a JSON object with the key "email" and an associated value of a String containing the entire email text
+    """
+    
+
+
+
+@app.route('/mailbox/email/<email_id:int>/folder', methods=['GET'])
+def get_email_folder():
+    """
+    Get the folder containing the given email.  Examples of folders include "Inbox", "Archive", "Trash", and "Sent".
+    """
+
+
+
+
+@app.route('/mailbox/email/<email_id:int>/labels', methods=['GET'])
+def get_email_labels():
+    """
+    Returns a JSON object with the fields "email_id" and "labels".  The value for labels is a list of strings.  Valid labels include "spam", "read", and "important".  No label may be repeated.
+    """
+
+
+
+
+@app.route('/mailbox/folder/<folder:str>', methods=['GET'])
+def get_mailbox_folder():
+    """
+    Lists the emails in a given folder.  Returns a list of email_ids.
+    """
+
+
+
+@app.route('/mailbox/labels/<label:str>', methods=['GET'])
+def get_mailbox_labels():
+    """
+    List emails with the given label.  Returns a list of email_ids.
+    """
+
+
+
+
+@app.route('/mailbox/email/<email_id:int>/folder/<folder:str>', methods=['PUT'])
+def put_email_to_folder():
+    """
+    Moves email to the given folder.  Folders include "Inbox", "Archive", "Trash", and "Sent".
+    """
+
+
+
+
+@app.route('/mailbox/email/<email_id:int>/label/<label:str>', methods=['PUT'])
+def mark_email_with_label():
+    """
+    Mark the given email with the given label. Valid labels include "spam", "read", and "important".
+    """
+
+
+
+
+@app.route('/mailbox/email/<email_id:int>/label/<label:str>', methods=['DELETE'])
+def remove_label_from_email():
+    """
+    Remove the given label from the given email. Valid labels include "spam", "read", and "important".
+    """
+
+
+
+
 app.run(port=5000)
+
 # print(get_db_connection())
     
