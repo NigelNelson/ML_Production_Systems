4628275176be2d83dae7c722222036252c6b92de
Nelson <nelsonni@msoe.edu>
2022-12-15 16:02:43 -0600

    Updated Minio client and log collection

--- Lab2/log_collection.py
+++ Lab2/log_collection.py
@@ -1,18 +1,58 @@
-#from pygtail import Pygtail
+from datetime import datetime
+from io import BytesIO
+from time import sleep
+
+from pygtail import Pygtail
 from minio import Minio
+from minio.error import InvalidResponseError
+
+import json
 
 
-client = Minio(
-        "http://localhost:9000 ",
+def read_log(tail):
+    data = bytearray(b'')
+    for line in tail:
+        log = json.loads(line)
+        data += json.dumps(log, ensure_ascii=False).encode('utf8')
+        data += "\n".encode('UTF-8')
+    return data
+
+def write_minio(data, minio_client):
+    object_name = datetime.now().strftime("%Y-%m-%d_%H-%M") + '.json'
+    try:
+        minio_client.put_object("log-files",
+                                    object_name, 
+                                    data=BytesIO(data),
+                                    length=len(data), 
+                                    content_type='application/json')
+        print("File uploaded successfully!")
+    except InvalidResponseError as err:
+        print(err)
+
+
+
+if __name__ == "__main__":
+
+    # Create Minio client
+    minio_client = Minio(
+        endpoint="localhost:9000",
+        secure=False,
         access_key="log-depositor",
-        secret_key="minioadmin",
+        secret_key="minioadmin"
     )
 
+    # Ensure that neccesary bucket exists
+    bucket_exists = minio_client.bucket_exists("log-files")
+    assert bucket_exists, "'log-files' bucket does not exist"
 
-found = client.bucket_exists("log-files")
-if not found:
-    print("connected")
-else:
-    print("not connected")
-# for line in Pygtail("log_file.json"):
-#     sys.stdout.write(line)
+    tail = Pygtail("log_file.json")
+
+    while True:
+        data = bytearray(b'')
+        while datetime.now().minute not in list(range(0,60,15)):
+            data += read_log(tail)
+            sleep(15)
+        if data:
+            write_minio(data, minio_client)
+            sleep(15)
+        
