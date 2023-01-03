edf763254fa0b078e469866f8284287c6ed6cd11
Nelson <nelsonni@msoe.edu>
2023-01-03 15:12:29 -0600

    Updated log_collection and README

--- Lab2/log_collection.py
+++ Lab2/log_collection.py
@@ -7,6 +7,8 @@ from minio import Minio
 from minio.error import InvalidResponseError
 
 import json
+import os
+
 
 
 def read_log(tail):
@@ -17,15 +19,15 @@ def read_log(tail):
         data += "\n".encode('UTF-8')
     return data
 
-def write_minio(data, minio_client):
+def write_minio(data, minio_client, bucket):
     object_name = datetime.now().strftime("%Y-%m-%d_%H-%M") + '.json'
     try:
-        minio_client.put_object("log-files",
-                                    object_name, 
-                                    data=BytesIO(data),
-                                    length=len(data), 
-                                    content_type='application/json')
-        print("File uploaded successfully!")
+        minio_client.put_object(bucket,
+                                object_name, 
+                                data=BytesIO(data),
+                                length=len(data), 
+                                content_type='application/json')
+        print(f'File {object_name} uploaded successfully!')
     except InvalidResponseError as err:
         print(err)
 
@@ -33,19 +35,26 @@ def write_minio(data, minio_client):
 
 if __name__ == "__main__":
 
+    MINIO_ACESS_KEY=os.environ.get('MINIO_ACESS_KEY')
+    MINIO_SECRET_KEY=os.environ.get('MINIO_SECRET_KEY')
+    MINIO_ENDPOINT=os.environ.get('MINIO_ENDPOINT')
+    MINIO_BUCKET=os.environ.get('MINIO_BUCKET')
+    LOG_FILE_PATH=os.environ.get('LOG_FILE_PATH')
+
     # Create Minio client
     minio_client = Minio(
-        endpoint="localhost:9000",
+        endpoint=MINIO_ENDPOINT,
         secure=False,
-        access_key="log-depositor",
-        secret_key="minioadmin"
+        access_key=MINIO_ACESS_KEY,
+        secret_key=MINIO_SECRET_KEY
     )
 
     # Ensure that neccesary bucket exists
-    bucket_exists = minio_client.bucket_exists("log-files")
-    assert bucket_exists, "'log-files' bucket does not exist"
+    bucket_exists = minio_client.bucket_exists(MINIO_BUCKET)
+    assert bucket_exists, f'{MINIO_BUCKET} bucket does not exist'
 
-    tail = Pygtail("log_file.json")
+    # tail the log file
+    tail = Pygtail(LOG_FILE_PATH)
 
     while True:
         data = bytearray(b'')
@@ -53,6 +62,6 @@ if __name__ == "__main__":
             data += read_log(tail)
             sleep(15)
         if data:
-            write_minio(data, minio_client)
+            write_minio(data, minio_client, MINIO_BUCKET)
             sleep(15)
         
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ML_Production_Systems
+
+## How to run:
+- Start Postgres if not already running:
+  - pg_ctl start -D "C:\Program Files\PostgreSQL\15\data"
+- start email service:
+  - POSTGRES_DATABASE=email_ingestion POSTGRES_USERNAME=ingestion_service POSTGRES_PASSWORD='puppet-soil-SWEETEN POSTGRES_HOST=127.0.0.1 python email_service.py
+- Start log collection:
+  - MINIO_ACESS_KEY=log-depositor MINIO_SECRET_KEY=minioadmin MINIO_ENDPOINT=localhost:9000 MINIO_BUCKET=log-files LOG_FILE_PATH=./log_file.json python log_collection.py
