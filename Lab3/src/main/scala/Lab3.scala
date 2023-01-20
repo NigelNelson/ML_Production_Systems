16770e3161e500a3ee90411ae8c666c131cae73a
Nelson <nelsonni@msoe.edu>
2023-01-19 22:15:37 -0600

    Complete project 3

--- Lab3/src/main/scala/Lab3.scala
+++ Lab3/src/main/scala/Lab3.scala
@@ -10,6 +10,10 @@ object Lab3 extends  App {
     .master("local[1]")
     .getOrCreate()
 
+  spark.sparkContext.setLogLevel("ERROR")
+
+
+  // Set S3 access properties
   spark.sparkContext
     .hadoopConfiguration.set("fs.s3a.access.key", "training_service")
   spark.sparkContext
@@ -19,13 +23,7 @@ object Lab3 extends  App {
   spark.sparkContext
     .hadoopConfiguration.set("fs.s3a.connection.ssl.enabled", "false")
 
-
-
-  spark.sparkContext.setLogLevel("ERROR")
-
-  /*
-   Read logs
-    */
+  // Read logs from Minio
   val df_original: DataFrame = spark
     .read
     .json("s3a://log-files/*.json")
@@ -35,38 +33,27 @@ object Lab3 extends  App {
   // filter out by label === spam
   val df_spam: Dataset[Row] = df_original
     .filter(df_original("label") === "spam")
-  println("Spam only filter df length")
+  println("Spam only filter df length:")
   println(df_spam.count())
 
-
-  /*
-  Read emails from Postgres - not working yet
-  https://spark.apache.org/docs/latest/sql-data-sources-jdbc.html
-  https://hevodata.com/learn/spark-postgresql/
-
-   */
-
+  // Define Postgres properties
   val url = "jdbc:postgresql://127.0.0.1:5432/email_ingestion"
   val tableName = "emails"
   val props = new Properties()
   props.setProperty("user", "ingestion_service")
   props.setProperty("password", "puppet-soil-SWEETEN")
 
-  // read table into DataFrame
+  // Read Postgres table into DataFrame
   val tableDf: DataFrame = spark.read
     .jdbc(url, tableName, props)
+  println("Postgres df email count:")
   println(tableDf.count())
 
-  val joinedDf = tableDf.join(df_spam, tableDf("email_id") === df_spam("email_id"), "left_outer").drop(df_spam.id)
+  // Join the Postgres dataframe table and the spam table
+  val joinedDf = tableDf.join(df_spam, Seq("email_id"), "left_outer").dropDuplicates(Seq("email_id"))
   println("Joined table count:")
   println(joinedDf.count())
 
-  val allColumnNames=joinedDf.columns
-  println(allColumnNames.mkString(","))
-
-  joinedDf.write.json("s3a://emails/test")
-
-
-
-
+  // Write joined df to Minio
+  joinedDf.coalesce(1).write.mode("overwrite").json("s3a://emails/ingestion_service")
 }
